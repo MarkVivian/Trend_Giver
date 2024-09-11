ca55aeb4682d332b0ded24fe81a9552eaf63bfdc
Mark Vivian <markviviantech@gmail.com>
2024-09-11 20:12:54 +0300

    fuck this shit im out

--- Web-Scrapper/main.py
+++ Web-Scrapper/main.py
@@ -3,14 +3,19 @@ from scrapper.useful import check_internet
 from time import sleep
 
 if __name__ == '__main__':
-    i = 4
-    while i <= 10:
-        # check if there is internet connection
+    retries = 0
+    max_retries = 5
+    i = 1
+    while retries < max_retries:
         if check_internet():
-            logger.DataLogger(f"https://backiee.com/search/sports+car?category=car&page={i}").runner()
-            i += 1
-        else:
-            sleep(input("type a time or leave it until internet access : "))
-            logger.DataLogger(f"https://backiee.com/search/sports+car?category=car&page={i}").runner()
-            i += 1
+            while i <= 10:
+                # check if there is internet connection
+                if check_internet():
+                    logger.DataLogger(f"https://backiee.com/search/sports+car?category=car&page={i}").runner()
+                    i += 1
+                else:
+                    while not check_internet():
+                        sleep(input("type a time or leave it until internet access : "))
+                        logger.DataLogger(f"https://backiee.com/search/sports+car?category=car&page={i}").runner()
+                    i += 1
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
